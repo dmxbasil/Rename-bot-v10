67ad0c1a4e15bbeecd0de9b6283f4c0db1f4cb43
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-12 14:47:32 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,16 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/MrMKN/TG-RENAMER-BOT-V5.git /TG-RENAMER-BOT-V5     
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /TG-RENAMER-BOT-V5
+fi
+cd /TG-RENAMER-BOT-V5
+pip3 install -U -r requirements.txt
+echo """
+------------------------
+| ⚡️ Bot is Starting ⚡️|
+------------------------
+"""
+python3 main.py
