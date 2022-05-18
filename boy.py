b9a15fb69495a50d2358f750056454483c3bdc51
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-18 12:14:48 +0530

    Update boy.py

--- boy.py
+++ boy.py
@@ -5,14 +5,12 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            "renamer",
-            session_name=Config.SESSION_NAME,
+            "telegram",                        
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
             bot_token=Config.BOT_TOKEN,
             workers=50,
             plugins={"root": "helpers"},
-            sleep_threshold=5,
         )
 
 bot = Bot()
