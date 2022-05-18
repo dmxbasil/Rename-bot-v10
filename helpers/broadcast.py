bd92c3a071912b4b88320cc2d6d0182e824c557a
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-18 12:02:37 +0530

    Update broadcast.py

--- helpers/broadcast.py
+++ helpers/broadcast.py
@@ -35,7 +35,7 @@ async def send_msg(user_id, message):
         return 500, f"{user_id} : {traceback.format_exc()}\n"
 
 
-@Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
+@Client.on_message(filters.command("broadcast") & filters.user(Config.BOT_OWNER) & filters.reply)
 async def broadcast_handler(m: Message):
     all_users = await db.get_all_users()
     broadcast_msg = m.reply_to_message
