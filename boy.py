f1c5ac3f89b5489c9cacf1b9364653d59b257a44
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-18 14:18:35 +0530

    Update boy.py

--- boy.py
+++ boy.py
@@ -3,21 +3,21 @@ from configs import Config
 
 
 ##################
-class App(Client):
+#class App(Client):
 
-    def __init__(self):
-        super().__init__(
-            name="telegram",                        
-            api_id=Config.API_ID,
-            api_hash=Config.API_HASH,
-            bot_token=Config.BOT_TOKEN,
-            workers=50,
-            plugins={"root": "helpers"},
-            sleep_threshold=5,
-        )
+    #def __init__(self):
+        #super().__init__(
+            #name="telegram",                        
+            #api_id=Config.API_ID,
+            #api_hash=Config.API_HASH,
+            #bot_token=Config.BOT_TOKEN,
+            #workers=50,
+            #plugins={"root": "helpers"},
+            #sleep_threshold=5,
+        #)
 
-bot = App()
-bot.run()
+#bot = App()
+#bot.run()
 #####################
 
 
