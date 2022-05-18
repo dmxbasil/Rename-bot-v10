ef0f236cfa3fd6e4fd4d397d4ff2c5edce1e50c8
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-18 11:58:44 +0530

    Update and rename main.py to boy.py

---
+++ boy.py
@@ -0,0 +1,19 @@
+from pyrogram import Client
+from configs import Config
+
+class Bot(Client):
+
+    def __init__(self):
+        super().__init__(
+            "renamer",
+            session_name=Config.SESSION_NAME,
+            api_id=Config.API_ID,
+            api_hash=Config.API_HASH,
+            bot_token=Config.BOT_TOKEN
+            workers=50,
+            plugins={"root": "helpers"},
+            sleep_threshold=5,
+        )
+
+bot = Bot()
+bot.run()
