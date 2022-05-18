0353939744377ba647cf7a5d4d7cea3f82f416fe
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-18 21:25:47 +0530

    Update boy.py

--- boy.py
+++ boy.py
@@ -25,7 +25,6 @@ from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, Peer
 
 
 MKN = Client(
-    session_name=Config.SESSION_NAME,
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
     bot_token=Config.BOT_TOKEN
