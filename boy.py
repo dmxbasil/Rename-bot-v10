5243b0d4e4f41a6df3bb964bf199008259fda466
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-18 14:48:53 +0530

    Update boy.py

--- boy.py
+++ boy.py
@@ -10,9 +10,7 @@ class App(Client):
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
             bot_token=Config.BOT_TOKEN,
-            workers=50,
             plugins={"root": "helpers"},
-            sleep_threshold=5,
         )
 
 bot = App()
