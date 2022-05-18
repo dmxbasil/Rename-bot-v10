39fe528b4eada5eb5831bb509d5bba2dae4fe0ee
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-18 11:08:15 +0530

    Update bcast_db.py

--- helpers/database/bcast_db.py
+++ helpers/database/bcast_db.py
@@ -8,20 +8,12 @@ db = mongo[DB_NAME]
 dbcol = db["user"]
 
 def insert(chat_id):
-    user_id = int(chat_id)
+    user_id = int(id)
     user_det = {"_id": user_id,"file_id": None, "caption": None}
     try:
       dbcol.insert_one(user_det)
     except:
       pass
-
-def find(chat_id):
-    id =  {"_id":chat_id}
-    x = dbcol.find(id)
-    for i in x:
-         thumb = i["file_id"]
-         caption = i["caption"]
-         return [thumb, caption]
 
 def getid():
     values = []
