26346f60246e1bfc0d3284e741823e406557bece
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-18 09:58:56 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -10,7 +10,7 @@ class Config(object):
     SESSION_NAME = os.environ.get("SESSION_NAME", "Rename-Bot-0")
     SLEEP_TIME = int(os.environ.get("SLEEP_TIME", 5))
     BOT_OWNER = int(os.environ.get("BOT_OWNER", 1287407305))
-    CAPTION = "Rename Bot by @{}\n\nMade by @KDramasFlix"
+    CAPTION = "ㅤ "
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100))
     MONGODB_URI = os.environ.get("MONGODB_URI", "")
