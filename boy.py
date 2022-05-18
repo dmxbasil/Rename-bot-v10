6f8dec202d6c4eaf6ecbf6f53da86626214462ce
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-18 12:42:40 +0530

    Update boy.py

--- boy.py
+++ boy.py
@@ -5,7 +5,7 @@ class RenameBot(Client):
 
     def __init__(self):
         super().__init__(
-            "telegram",                        
+            name="telegram",                        
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
             bot_token=Config.BOT_TOKEN,
